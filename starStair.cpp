58a2195c973335d97400da67bcc80671a89d07e1
mezan <mrmezan0@gmail.com>
2019-12-21 06:25:15 +0600

    Full code of star stairs problem

--- starStair.cpp
+++ starStair.cpp
@@ -1,3 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the staircase function below.
 void staircase(int n) {
     for(int i=n;i>0;i--){
         for(int j=1;j<=i;j++){
@@ -15,4 +20,15 @@ void staircase(int n) {
         cout<<"\n";
     }
 
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    staircase(n);
+
+    return 0;
 }
