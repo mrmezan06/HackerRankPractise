bbda3623c47a6a2c58e7f41de428f4916bfd4054
mezan <mrmezan0@gmail.com>
2019-12-21 07:13:17 +0600

    Birthday Cake Candle Solve but TLE Problem

---
+++ birthdaycakecandle_me_solve_TLE.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+        int temp = 0;
+        int max = 0;
+        
+        for(int i=0;i<ar.length; i++){
+            for(int j=0;j<ar.length;j++){
+                if(ar[i]==ar[j]){
+                    temp++;
+                }
+            }
+            if(temp>max){
+                        max = temp;
+                    }
+                    temp = 0;
+        }
+        
+        return max;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
